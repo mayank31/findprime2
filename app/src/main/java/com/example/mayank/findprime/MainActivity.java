ccd6ffd81157e82d4987c0672cd9d4bddf191a5e
mayank31 <mayank16030@iiitd.ac.in>
2016-08-18 10:55:55 +0530

    trial

---
+++ app/src/main/java/com/example/mayank/findprime/MainActivity.java
@@ -0,0 +1,131 @@
+    package com.example.mayank.findprime;
+
+import android.content.Intent;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.Display;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button yes;
+    private Button no;
+    private Button next;
+    private TextView ques;
+
+    int randno;
+    private static final String TAG = "MainActivity";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    ques = (TextView) findViewById(R.id.ques);
+    yes = (Button) findViewById(R.id.yes);
+    no = (Button) findViewById(R.id.no);
+    next = (Button) findViewById(R.id.next);
+
+    final Random r = new Random();
+        randno = r.nextInt(1000-0) % 1001;
+    ques.setText("Is " + randno + " a prime number?");
+    yes.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (checkprime(randno) == true) {
+                Toast.makeText(getApplicationContext(), "Correct ans", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "InCorrect ans", Toast.LENGTH_LONG).show();
+            }
+        }
+    });
+    no.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            if (checkprime(randno) == true) {
+                Toast.makeText(getApplicationContext(), "InCorrect ans", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(getApplicationContext(), "Correct ans", Toast.LENGTH_SHORT).show();
+            }
+        }
+    });
+    next.setOnClickListener(new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+           // Random r1 = new Random();
+            randno = r.nextInt(1000-0) % 1001;
+            ques.setText("Is " + randno + " a prime number?");
+        }
+    });
+
+    }
+
+    @Override
+    public void onSaveInstanceState(Bundle savedInstanceState){
+        super.onSaveInstanceState(savedInstanceState);
+        Log.i(TAG, "Inside onSaveInstance");
+    }
+
+    @Override
+    public void onStart()
+    {
+        super.onStart();
+        Log.d(TAG, "Inside OnStart");
+    }
+
+    @Override
+    public void onPause()
+    {
+        super.onPause();
+        Log.d(TAG,"Inside OnPause");
+    }
+
+    @Override
+    public void onResume(){
+        super.onResume();
+        Log.d(TAG, "Inside OnREsume");
+
+    }
+
+    @Override
+    public void onStop(){
+        super.onStop();
+        Log.d(TAG, "Inside OnSTop");
+    }
+
+    @Override
+    public void onDestroy(){
+        super.onDestroy();
+        Log.d(TAG, "Inside OnDestroy");
+    }
+    public boolean checkprime(int no)
+    {
+        boolean ans;
+        ans=true;
+        int i=0;
+        for(i=2;i<=no/2;i++)
+        {
+            if((no%i)==0)
+            {
+                ans=false;
+                 break;
+            }
+        }
+        return ans;
+    }
+
+}
